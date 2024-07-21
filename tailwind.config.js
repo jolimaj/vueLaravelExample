c107e9aeacae4b9dea1749c646f6d0c969842d6f
Jolima <javinjolima@gmail.com>
2024-07-21 16:15:51 +0800

    build: Added config for color scheme and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,28 @@ import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
-    ],
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './vendor/laravel/jetstream/**/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.vue',
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Verdana', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'blue': '#00138C',
+        'orange-gradient-top': '#F79D25',
+        'orange-gradient-bottom': '#F05E16', 
+        'blue-gradient-top': '#0044ee',
+        'blue-gradient-bottom': '#00138c'
+      },
     },
+  },
 
-    plugins: [forms, typography],
+  plugins: [forms, typography],
 };
